6e3675ed56d0903de8f1cc79be42e992cbde50fc
Thomas Randall <36749093+tlranda@users.noreply.github.com>
2022-04-06 12:12:13 -0400

    Setup should not re-install sacrebleu

--- setup.sh
+++ setup.sh
@@ -18,7 +18,6 @@ echo "Validate presence of WMT14 de-en"
 python -c "import datasets; print(datasets.load_dataset('wmt14','de-en'))"
 
 # SacreBLEU Dependency for BLEU scores
-pip3 install --user sacrebleu
 # Validation
 python -c "import datasets; print('SacreBleu OK?', datasets.load_metric('sacrebleu') is not None)"
 
